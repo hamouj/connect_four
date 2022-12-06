fdf6f996a4ea8a14ad885b10e59eed3065941380
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-12-06 15:31:36 -0800

    Test: create tests for valid_move? method

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,4 +4,42 @@ class Turn
         @board = board
         @player = player
     end
+
+    def valid_move?(input)
+        if input == 'A'
+            board['A'].any? do |column|
+                column.include?('.')
+                'Invalid move' if false
+            end 
+        elsif input == 'B'
+            board['B'].any? do |column|
+                column.include?('.')
+                'Invalid move' if false
+            end 
+        elsif input == 'C'
+            board['C'].any? do |column|
+                column.include?('.')
+                'Invalid move' if false
+            end 
+        elsif input == 'D'
+            board['D'].any? do |column|
+                column.include?('.')
+                'Invalid move' if false
+            end 
+        elsif input == 'E'
+            board['E'].any? do |column|
+                column.include?('.')
+                'Invalid move' if false
+            end 
+        elsif input == 'F'
+            board.all_columns[5].any? do |column|
+                column.include?('.')
+                'Invalid move' if false
+            end 
+        elsif input == 'G'
+            board.all_columns[6].any? do |column|
+                column.include?('.')                
+                'Invalid move' if false
+            end 
+    end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -4,9 +4,28 @@ describe Turn do
     describe '#initialize' do
         it 'is an instance of the Turn class' do
             # player = Player.new('Ivan')
-            turn = Turn.new("Ivan")
+            turn = Turn.new('Ivan')
 
             expect(turn).to be_a(Turn)
         end
     end
+
+    describe '#valid_move?' do
+        it 'determines the move is valid' do
+            # player = Player.new('Ivan')
+            turn = Turn.new('Ivan')
+            input = "B"
+            turn.valid_move?(input)
+
+            expect(turn.valid_move?).to be true
+        end
+
+        it 'determines the move is invalid' do
+             # player = Player.new('Ivan')
+            turn = Turn.new('Ivan')
+            # input = 
+            turn.valid_move?(input)
+            
+            expect(turn.valid_move?).to eq('Invalid move')
+        end 
 end
