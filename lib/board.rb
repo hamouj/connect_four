63d5a0f669acfc36b862e047b7c9099c4c59e2a2
Axel De La Guardia <axeldelaguardia@gmail.com>
2022-12-05 18:04:50 -0700

    Create #board_position methods with 42 periods in an array

--- lib/board.rb
+++ lib/board.rb
@@ -2,4 +2,14 @@ class Board
     def initialize
     
     end
+
+    def board_positions
+        positions = []
+        puts 'A B C D E F G'
+        42.times do
+            positions << '.'
+        end
+        positions
+    end
+    
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,10 +3,20 @@ require 'rspec'
 
 describe Board do
     describe 'initialize' do
+
         it 'is an instance of Board' do
             board = Board.new
             
             expect(board).to be_a(Board)
         end
+
+        it '#board_positions' do
+            board = Board.new
+            expect(board.board_positions.count).to eq(42)
+        end
+
+        
+
     end
+
 end
