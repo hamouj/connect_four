c1ef3c46e8fbea7b21859fc89a31648e358c0442
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-12-06 20:59:53 -0800

    Test: create and pass tests for place_piece methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,11 +10,22 @@ class Turn
         column_space = board[input].any? do |column|
             column.include? ('.')
         end 
-            
             if column_space == false
                 'That column is full. Choose a different column.'
             else
                 'You are good to go!'
             end     
     end 
+
+    def player_place_piece(input)
+        board[input].find do |column|
+            column.replace('X') if column.include?('.')
+        end
+    end
+
+    def computer_place_piece(input)
+        board[input].find do |column|
+            column.replace('O') if column.include?('.')
+        end
+    end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,6 +1,7 @@
 require './lib/turn'
 # require './lib/player'
 # require './lib/board'
+# require './lib/computer'
 
 describe Turn do
     describe '#initialize' do
@@ -56,5 +57,43 @@ describe Turn do
             
             expect(turn.valid_move?(input)).to eq('That column is full. Choose a different column.')
         end 
+    end
+
+    describe '#place_piece methods' do
+        it 'places an X in the chosen column for a person move' do
+            board = {
+                'A' => ['X', 'O', 'O', 'X', 'O', 'X', 'O'],
+                'B' => ['.', '.', '.', '.', '.', '.', '.'],
+                'C' => ['.', '.', '.', '.', '.', '.', '.'],
+                'D' => ['.', '.', '.', '.', '.', '.', '.'],
+                'E' => ['.', '.', '.', '.', '.', '.', '.'],
+                'F' => ['.', '.', '.', '.', '.', '.', '.'],
+                'G' => ['.', '.', '.', '.', '.', '.', '.']  
+                }  
+                turn = Turn.new('Ivan', board)
+                input = 'B'
+                turn.valid_move?(input)
+                turn.player_place_piece(input)
+
+                expect(board['B'][0]).to eq('X')
+        end
+
+        it 'places an O in the chosen column for a computer move' do
+            board = {
+                'A' => ['X', 'O', 'O', 'X', 'O', 'X', 'O'],
+                'B' => ['.', '.', '.', '.', '.', '.', '.'],
+                'C' => ['.', '.', '.', '.', '.', '.', '.'],
+                'D' => ['.', '.', '.', '.', '.', '.', '.'],
+                'E' => ['.', '.', '.', '.', '.', '.', '.'],
+                'F' => ['X', '.', '.', '.', '.', '.', '.'],
+                'G' => ['.', '.', '.', '.', '.', '.', '.']  
+                }  
+                turn = Turn.new('Ivan', board)
+                input = 'F'
+                turn.valid_move?(input)
+                turn.computer_place_piece(input)
+
+                expect(board['F'][1]).to eq('O')
+        end
     end
 end
