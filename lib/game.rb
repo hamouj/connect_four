4cbabe859b0279e88da2b2e2fed9b3f7ce6efd9f
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-12-09 07:48:36 -0800

    Feature: Update #play_game and #start, add #end_game and #play_again methods

--- lib/game.rb
+++ lib/game.rb
@@ -3,8 +3,9 @@ class Game
     attr_accessor :player
 
     def initialize
+        @computer = Computer.new
         @player = Player.new('name')
-        @board = Board.new(@player)
+        @board = Board.new(@player, @computer)
         @turn = Turn.new(@board)
     end
     
@@ -14,6 +15,10 @@ class Game
 
         begin_game = gets.strip
         if begin_game.downcase == 'p'
+            puts "------------------------------------------------------"
+            p "Lets play! First, what is your name?"
+            player.name = gets.strip
+            puts "------------------------------------------------------"
             instructions
         else
             exit
@@ -21,10 +26,6 @@ class Game
     end
 
     def instructions
-        puts "------------------------------------------------------"
-        p "Lets play! First, what is your name?"
-        player.name = gets.strip
-        puts "------------------------------------------------------"
         p "#{player.name}, you will be assigned the piece 'X'. Good luck!"
         puts "------------------------------------------------------"
         board.create_board
@@ -32,20 +33,47 @@ class Game
     end
 
     def play_game
-        p 'Choose a column (A-G). Type the letter.'
-        board.print_board
-        player.input = gets.strip
-        until player.valid_input? == true
+       until board.winner == computer || board.winner == player || board.full? == true
+            p 'Choose a column (A-G). Type the letter.'
+            board.print_board
             player.input = gets.strip
+            until player.valid_input? == true
+                player.input = gets.strip
+            end
+            input = player.input.upcase
+            turn.player_valid_move?(input)
+            puts "------------------------------------------------------"
+            p "Computer's move"
+            board.computer.give_input
+            input = board.computer.input
+            turn.computer_valid_move?(input)
         end
-        input = player.input.upcase
-        turn.player_valid_move?(input)
-        puts "------------------------------------------------------"
-        p "Computer's move"
-        board.computer.give_input
-        input = board.computer.input
-        turn.computer_valid_move?(input)
-        end
+            end_game
     end
 
+    def end_game
+        if board.winner == player
+            p "#{player.name} won--nice job!"
+        elsif board.winner == computer
+            p "Computer won this round."
+        elsif board.full? == true
+            p "Draw!"
+        end 
+        play_again?
+    end 
+
+    def play_again?
+        p "Would you like to play again? Type 'yes' or 'no'."
+        answer = 'j'
+        until answer.downcase == 'yes' || answer.downcase == 'no' 
+            answer = gets.strip
+            if answer.downcase == 'yes'
+                instructions
+            elsif answer.downcase =='no'
+                exit
+            else 
+                p "Didn't get that. Enter 'yes' or 'no'."
+            end
+        end    
+    end 
 end 
