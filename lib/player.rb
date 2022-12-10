fc5246daca911fba6a2c98073d9aa7ead813729a
Axel De La Guardia <axeldelaguardia@gmail.com>
2022-12-10 15:57:17 -0700

    Add: return value to typing exit

--- lib/player.rb
+++ lib/player.rb
@@ -9,12 +9,13 @@ class Player
 	end
     
 	def valid_input?
-		valid_inputs = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'EXIT']
+		valid_inputs = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 		if valid_inputs.include?(@input.upcase)
 			true
 		elsif @input.upcase == 'EXIT'
 			p 'See you later!'
 			exit
+			return 'See you later!'
 		else
 			p 'Typo? It happens to the best of us!'
 			p 'Type the letter of the column you would like to play (A-G).'
