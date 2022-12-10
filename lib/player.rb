1f2f945a6490b2b073f5cd0f4a34ef9fac16d20c
Axel De La Guardia <axeldelaguardia@gmail.com>
2022-12-09 19:05:19 -0700

    Fix: Tab spacing

--- lib/player.rb
+++ lib/player.rb
@@ -1,19 +1,19 @@
 class Player
-    attr_reader :piece
-    attr_accessor :input, :name
-    def initialize(name)
-        @name = name
-        @input = nil
-        @piece = 'X'
-    end
+	attr_reader :piece
+	attr_accessor :input, :name
+	def initialize(name)
+		@name = name
+		@input = nil
+		@piece = 'X'
+	end
     
-    def valid_input?
-        valid_inputs = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-        if valid_inputs.include?(@input.upcase)
-            true
-        else
-            p 'Typo? It happens to the best of us!'
-            p 'Type the letter of the column you would like to play (A-G).'
-        end
-    end
+	def valid_input?
+		valid_inputs = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+		if valid_inputs.include?(@input.upcase)
+			true
+		else
+			p 'Typo? It happens to the best of us!'
+			p 'Type the letter of the column you would like to play (A-G).'
+		end
+	end
 end
