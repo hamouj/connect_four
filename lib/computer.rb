47ae54143a00594e7e594b547f530c2d69d5c097
Axel De La Guardia <axeldelaguardia@gmail.com>
2022-12-09 19:07:05 -0700

    Fix: Tab spacing

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,12 +1,12 @@
 class Computer
-    attr_reader :input, :piece
-    def initialize
-        @input = nil
-        @piece = "O"
-    end
+	attr_reader :input, :piece
+	def initialize
+		@input = nil
+		@piece = "O"
+	end
 
-    def give_input
-        valid_input = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-        @input = valid_input.sample
-    end
+	def give_input
+		valid_input = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+		@input = valid_input.sample
+	end
 end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,19 +1,19 @@
 require './lib/computer'
 
 describe Computer do
-    describe '#initialize' do
-        it 'is an instance of the Computer class'do
-            computer = Computer.new
+	describe '#initialize' do
+		it 'is an instance of the Computer class'do
+			computer = Computer.new
 
-            expect(computer).to be_a(Computer)
-        end
-    end
+			expect(computer).to be_a(Computer)
+		end
+	end
 
-    describe '#give_input' do
-        it 'generates input' do
-            computer = Computer.new
+	describe '#give_input' do
+		it 'generates input' do
+			computer = Computer.new
 
-            expect(['A', 'B', 'C', 'D', 'E', 'F', 'G']). to include(computer.give_input)
-        end
-    end
+			expect(['A', 'B', 'C', 'D', 'E', 'F', 'G']). to include(computer.give_input)
+		end
+	end
 end
